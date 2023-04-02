fa00bdd5e14ca3f2777fbab32154dcac48113f49
Malthemester <malthemester@gmail.com>
2023-04-02 13:00:06 +0200

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -11,57 +11,66 @@ import Item from './Pages/Item'
 import NewItem from './Pages/NewItem'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './Components/AuthProvider'
+import EditItem from './Pages/EditItem'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+   uri: 'http://localhost:4000/graphql',
+   cache: new InMemoryCache()
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const routes = [
-  {
-    path: '/',
-    element: (
-      <NavBar>
-        <Landing />
-      </NavBar>
-    )
-  },
-  {
-    path: '/items',
-    element: (
-      <NavBar>
-        <Items />
-      </NavBar>
-    )
-  },
-  {
-    path: '/item/:id',
-    element: (
-      <NavBar>
-        <Item />
-      </NavBar>
-    )
-  },
-  {
-    path: '/item/new',
-    element: (
-      <NavBar>
-        <NewItem />
-      </NavBar>
-    )
-  }
+   {
+      path: '/',
+      element: (
+         <NavBar>
+            <Landing />
+         </NavBar>
+      )
+   },
+   {
+      path: '/items',
+      element: (
+         <NavBar>
+            <Items />
+         </NavBar>
+      )
+   },
+   {
+      path: '/item/:id',
+      element: (
+         <NavBar>
+            <Item />
+         </NavBar>
+      )
+   },
+   {
+      path: '/item/new',
+      element: (
+         <NavBar>
+            <NewItem />
+         </NavBar>
+      )
+   },
+   {
+      path: '/item/edit/:id',
+      element: (
+         <NavBar>
+            <EditItem />
+         </NavBar>
+      )
+   }
 ]
 
 root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-    <AuthProvider>
-      <RouterProvider router={createBrowserRouter(routes)} />
-    </AuthProvider>
-    </ApolloProvider>
-  </React.StrictMode>
+   <React.StrictMode>
+      <ApolloProvider client={client}>
+         <AuthProvider>
+            <RouterProvider router={createBrowserRouter(routes)} />
+         </AuthProvider>
+      </ApolloProvider>
+   </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
